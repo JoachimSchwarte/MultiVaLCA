9733270ece5ec79dd38f694c93fc56cb27699e97
Joachim Schwarte <joachim.schwarte@iwb.uni-stuttgart.de>
2018-07-10 15:53:09 +0200

    Version 0.539 (7)

--- src/de/unistuttgart/iwb/multivalcagui/XMLExportAction.java
+++ src/de/unistuttgart/iwb/multivalcagui/XMLExportAction.java
@@ -61,6 +61,7 @@ class XMLExportAction extends AbstractAction {
             writeCFactors(document, root);
             writeLCIAMethods(document, root);
             writeProductDeclarations(document, root);
+            writeComponents(document, root);
             
             JFileChooser chooser = new JFileChooser();
 	        FileFilter filter = new FileNameExtensionFilter("XML-Dateien (*.xml)", "xml");
@@ -347,6 +348,21 @@ class XMLExportAction extends AbstractAction {
             	menge3.appendChild(document.createTextNode(thisPD.getImpactValueMap(thisPD.getBM()).get(ic).get(ValueType.UpperBound).toString()));
             }
         }		
+	}
+	
+	private void writeComponents(Document document, Element root) {
+        Element alleComponents = document.createElement("Components");
+        root.appendChild(alleComponents);
+        
+        for (String comName : Component.getAllInstances().keySet()) {
+        	Component thisCom = Component.getAllInstances().get(comName);
+        	Element com1 = document.createElement("Component");
+        	alleComponents.appendChild(com1);
+        	Element name = document.createElement("Component-Name");
+        	com1.appendChild(name);
+        	
+        }
+		
 	}
 	
 }
