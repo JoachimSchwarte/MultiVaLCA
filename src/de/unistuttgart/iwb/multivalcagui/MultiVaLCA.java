1b4dc3731e345485655d517eb93288ef1f60b921
Joachim Schwarte <joachim.schwarte@iwb.uni-stuttgart.de>
2023-06-16 15:20:55 +0200

    Version 0.806

--- src/de/unistuttgart/iwb/multivalcagui/MultiVaLCA.java
+++ src/de/unistuttgart/iwb/multivalcagui/MultiVaLCA.java
@@ -37,14 +37,14 @@ import net.miginfocom.swing.MigLayout;
 
 /**
  * @author Dr.-Ing. Joachim Schwarte, Helen Hein, Johannes Dippon
- * @version 0.805
+ * @version 0.806
  */
 
 public class MultiVaLCA {
 
 	private Language l = GUILanguage.getChosenLanguage();
-	private String versionString ="Version 0.805";
-	private String dateString ="06.06.2023";
+	private String versionString ="Version 0.806";
+	private String dateString ="16.06.2023";
 
 	private JFrame frame  = new JFrame();
 	private JPanel panel  = new JPanel();
